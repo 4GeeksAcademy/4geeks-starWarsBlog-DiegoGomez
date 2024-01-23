370c28ffd7c0b9dd328683d10326ae9706f19f09
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2024-01-23 21:39:06 +0100

    Estilo principal

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,26 @@
 import React from "react";
+import img from "../../img/boba-fett.png";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-dark text-white">
+      <div className="d-flex flex-column align-items-center">
+    <div className="d-flex align-items-center">
+        <img src={img} className="w-25" alt="Star Wars Logo" />
+        <h1 className="display-2 mt-5">Star Wars</h1>
+    </div>
+    <p>
+        Made by <a className="text-decoration-none" href="https://github.com/diegogomezgonza?tab=followers">diegogomezgonza</a>
+    </p>
+</div>
+
+
+      <div className="ml-auto p-4">
+        <button type="button" class="btn btn-warning btn-lg">
+          Favorites<i class="fa-solid fa-caret-down ms-3"></i>
+        </button>
+      </div>
+    </nav>
+  );
 };
